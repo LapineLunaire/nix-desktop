2437567f0bda691b1b3707a910a60989c8b45629
Carmilla <carmilla@lunaire.eu>
2026-08-16 08:31:49 +0200

    camellya: the samba shares on vault, mounted on first access

---
+++ hosts/camellya/samba-mounts.nix
@@ -0,0 +1,35 @@
+# CIFS mounts of the shares vault.lunaire.moe serves. Kept out of hardware-configuration.nix so regenerating that file does not drop them.
+{
+  config,
+  pkgs,
+  ...
+}: let
+  carmilla = config.users.users.carmilla;
+in {
+  # mount.cifs, which the mounts below need.
+  environment.systemPackages = [pkgs.cifs-utils];
+
+  fileSystems =
+    builtins.mapAttrs (_: share: {
+      device = "//vault.lunaire.moe/${share}";
+      fsType = "cifs";
+      options = [
+        "credentials=${config.sops.templates."samba-credentials".path}"
+        "uid=${toString carmilla.uid}"
+        "gid=${toString config.users.groups.${carmilla.group}.gid}"
+        "seal"
+        "nosuid"
+        "nodev"
+        "noexec"
+        "_netdev"
+        "x-systemd.automount"
+        "noauto"
+        "x-systemd.idle-timeout=60"
+        "x-systemd.device-timeout=5s"
+        "x-systemd.mount-timeout=5s"
+      ];
+    }) {
+      "/home/carmilla/vault" = "carmilla";
+      "/home/carmilla/torrents" = "torrents";
+    };
+}
