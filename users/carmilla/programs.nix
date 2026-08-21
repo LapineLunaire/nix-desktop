4fbe0dc511edb7983a7a8259dbbf231b3101a02e
Carmilla <carmilla@lunaire.eu>
2026-08-21 18:44:05 +0200

    carmilla: superpowers v6.2.0 -> v6.3.0

--- users/carmilla/programs.nix
+++ users/carmilla/programs.nix
@@ -15,8 +15,8 @@
     plugins.superpowers = pkgs.fetchFromGitHub {
       owner = "obra";
       repo = "superpowers";
-      rev = "44c9b2d6e889982ac18c27d05a19fefe335194e1";
-      hash = "sha256-fnl+HbPL2qD5Zgz8a1NctjFJSqu6UsyHJAhQMLQNXXc=";
+      rev = "b36e0829c6d0140e93cfef2ca599b1b07d4a7797";
+      hash = "sha256-EsGNO0dULWf5Bx6bGrCv2kI2Z8aKH0kRvGiuN23wChQ=";
     };
   };
 
